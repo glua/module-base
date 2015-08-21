1fad23719d8ff6052c7ba33cf1f703a5bc6b79c6
William Wallace <me@wiox.me>
2015-08-21 19:49:21 +0100

    Remove unnecessary premake5 directives

--- premake5.lua
+++ premake5.lua
@@ -1,22 +1,7 @@
-local target_suffixes = {
-
-	bsd       = "linux",
-	linux     = "linux",
-	solaris   = "linux",
-	windows   = "win32",
-	macosx    = "osx"  ,
-
-}
-
-
 solution "Example"
 	language		"C++"
 	location		"project"
-
 	targetdir		"bin"
-	targetprefix		"gm_"
-	targetextension		".dll"
-	targetsuffix		( "_" .. target_suffixes[ os.get() ] )
 
 	--
 	-- Statically link the C-Runtime to reduce dependencies needed to run our module
